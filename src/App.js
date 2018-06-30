498f98d98ee47b43e48ca6175c5dc76ec98e2d88
BradHick <bradleyhrm@gmail.com>
2018-06-29 23:21:24 -0300

    Creating App bar and Todo component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Todo from './todo/todo';
+import Bar from './template/bar';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div style={{flexGrow: 1, margin: '0 2px', transform: 'scale(0.8)'}} >
+        <Bar />
+        <Todo />
       </div>
     );
   }
