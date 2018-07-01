e733c1fae573c1cd058537cd1308cbaeb0e63f54
BradHick <bradleyhrm@gmail.com>
2018-06-30 22:53:26 -0300

    Config Reducers

--- src/todo/todo.js
+++ src/todo/todo.js
@@ -4,6 +4,12 @@ import TodoForm from './todoForm';
 import TodoList from './todoList';
 
 export default class Todo extends Component { 
+
+  constructor(props){
+    super(props);
+    this.handleAdd = this.handleAdd.bind(this);
+  };
+
   render() { 
     return ( 
       <div>
